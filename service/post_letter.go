f400b0823d0c2919c03ea62bf37bf7c2b95b3bfc
matsudaq <145122582+matsudaq@users.noreply.github.com>
2023-09-17 10:31:55 +0900

    time 0-padding

--- service/post_letter.go
+++ service/post_letter.go
@@ -13,6 +13,21 @@ type PostLetter struct {
 }
 
 func (a *PostLetter) PostLetter(ctx context.Context, letterContent *entity.Strokes) (string, error) {
+	var baseTime int64 = 9223372036854775807
+
+	for i := 0; i < len(*letterContent); i++ {
+		st := &((*letterContent)[i].Points)
+		for j := 0; j < len(*st); j++ {
+			baseTime = min(baseTime, (*st)[j].Time)
+		}
+	}
+	for i := 0; i < len(*letterContent); i++ {
+		st := &((*letterContent)[i].Points)
+		for j := 0; j < len(*st); j++ {
+			(*st)[j].Time -= baseTime
+		}
+	}
+
 	latj, err := json.Marshal(letterContent)
 	if err != nil {
 		return "", fmt.Errorf("PostLetter: Marshal error: %w", err)
